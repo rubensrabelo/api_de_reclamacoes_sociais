38ef779bd01c38b76e112e5eca19916729ffa7ca
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-04-30 10:23:08 -0300

    feat(dto): create DTOs for response, creation, and update of the complaint model

---
+++ src/main/java/com/project/complaints/data/dto/complaint/ComplaintResponseDTO.java
@@ -0,0 +1,129 @@
+package com.project.complaints.model;
+
+import com.project.complaints.model.embedded.Address;
+import com.project.complaints.model.enums.StatusEnum;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Document(collection = "complaints")
+public class Complaint implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private String id;
+    private String title;
+    private String description;
+    private Address address;
+    private StatusEnum status;
+    private String imageUrl;
+
+    @CreatedDate
+    private Instant createdDate;
+
+    @LastModifiedDate
+    private Instant updatedDate;
+    private boolean isAnonymous;
+
+    private Complaint() {
+        this.status = StatusEnum.PENDING;
+    }
+
+    public Complaint(String title, String description, Address address, String imageUrl, boolean isAnonymous) {
+        this.title = title;
+        this.description = description;
+        this.address = address;
+        this.imageUrl = imageUrl;
+        this.isAnonymous = isAnonymous;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public StatusEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusEnum status) {
+        this.status = status;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public Instant getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Instant createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Instant getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Instant updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+
+    public boolean isAnonymous() {
+        return isAnonymous;
+    }
+
+    public void setAnonymous(boolean anonymous) {
+        isAnonymous = anonymous;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Complaint complaint = (Complaint) o;
+        return isAnonymous == complaint.isAnonymous && Objects.equals(id, complaint.id) && Objects.equals(title, complaint.title) && Objects.equals(description, complaint.description) && Objects.equals(address, complaint.address) && status == complaint.status && Objects.equals(imageUrl, complaint.imageUrl) && Objects.equals(createdDate, complaint.createdDate) && Objects.equals(updatedDate, complaint.updatedDate);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, title, description, address, status, imageUrl, createdDate, updatedDate, isAnonymous);
+    }
+}
---
+++ src/main/java/com/project/complaints/data/dto/complaint/ComplaintUpdateDTO.java
@@ -0,0 +1,77 @@
+package com.project.complaints.data.dto.complaint;
+
+import com.project.complaints.model.embedded.Address;
+import com.project.complaints.model.enums.StatusEnum;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.hateoas.RepresentationModel;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+public class ComplaintCreateDTO {
+
+    @NotBlank
+    @Size(min=1, max = 100)
+    private String title;
+
+    @NotBlank
+    @Size(min=1, max = 255)
+    private String description;
+    private Address address;
+    private String imageUrl;
+    private boolean isAnonymous;
+
+    private ComplaintCreateDTO() {
+    }
+
+    public ComplaintCreateDTO(String title, String description, Address address, String imageUrl, boolean isAnonymous) {
+        this.title = title;
+        this.description = description;
+        this.address = address;
+        this.imageUrl = imageUrl;
+        this.isAnonymous = isAnonymous;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public boolean isAnonymous() {
+        return isAnonymous;
+    }
+
+    public void setAnonymous(boolean anonymous) {
+        isAnonymous = anonymous;
+    }
+}
