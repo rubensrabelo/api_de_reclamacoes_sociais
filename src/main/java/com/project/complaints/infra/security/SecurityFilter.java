699565ab955769075e9cb1dcd5492cee58d2d527
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-05-06 09:09:51 -0300

    feat(security): create security filter

---
+++ src/main/java/com/project/complaints/infra/security/SecurityFilter.java
@@ -0,0 +1,50 @@
+package com.project.complaints.infra.security;
+
+import com.project.complaints.model.User;
+import com.project.complaints.repository.UserRepository;
+import com.project.complaints.service.exceptions.ObjectNotFoundException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenService tokenService;
+    private final UserRepository userRepository;
+
+    public SecurityFilter(TokenService tokenService, UserRepository userRepository) {
+        this.tokenService = tokenService;
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        var token = recoverToken(request);
+        var login = tokenService.validateToken(token);
+
+        if(login != null) {
+            User user = userRepository.findByEmail(login)
+                    .orElseThrow(() -> new ObjectNotFoundException("User not found."));
+            var authorities = Collections.singleton(new SimpleGrantedAuthority("ROLE_USER"));
+            var authentication = new UsernamePasswordAuthenticationToken(user, login, authorities);
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String recoverToken(HttpServletRequest request) {
+        var authHeader = request.getHeader("Authorization");
+        if(authHeader == null)  return null;
+        return authHeader.replace("Bearer ", "");
+    }
+}
--- src/main/java/com/project/complaints/repository/UserRepository.java
+++ src/main/java/com/project/complaints/repository/UserRepository.java
@@ -3,5 +3,8 @@ package com.project.complaints.repository;
 import com.project.complaints.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends MongoRepository<User, String> {
+    Optional<User> findByEmail(String email);
 }
